cf51d56e95bb9a9a90a095df5f062f16975a9624
Niggi1105 <niklas.liesch17@gmail.com>
2023-01-29 20:36:18 +0100

    <add writeback>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::io::{BufReader, BufWriter};
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,6 +15,8 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                let mut writer = BufWriter::new(&_stream);
+                writer.write_all("+PONG\r\n".as_bytes()).expect("Cant write back");
             }
             Err(e) => {
                 println!("error: {}", e);
