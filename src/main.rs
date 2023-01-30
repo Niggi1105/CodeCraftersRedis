a6d4b434f5895b358d44283a057ebef3c0886177
Niggi1105 <niklas.liesch17@gmail.com>
2023-01-30 22:33:19 +0100

    <create loop for PING response>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
-// Uncomment this block to pass the first stage
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{Write,Read};
+use std::net::TcpStream;
+
+fn handle_connection(stream: &mut TcpStream){
+    loop{
+        let mut buffer = [0;128];
+        println!("listening fo incomming PING");
+        stream.read(&mut buffer).unwrap();
+        stream.write_all("+PONG\r\n".as_bytes()).unwrap();
+    }
+    println!("Exited loop");
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
+
                 println!("accepted new connection");
-                Write::write_all(&mut _stream,"+PONG\r\n".as_bytes()).expect("Cant write back");
+                
+                handle_connection(&mut _stream);
             }
             Err(e) => {
                 println!("error: {}", e);
