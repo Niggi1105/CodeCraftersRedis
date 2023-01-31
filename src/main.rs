7a9df0ecdfbde096681c7f137abec02f7da11ed7
Niggi1105 <niklas.liesch17@gmail.com>
2023-01-31 19:47:09 +0100

    <add asynchrone handling>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,39 @@
 use std::net::TcpListener;
 use std::io::{Write,Read};
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+use std::net::Shutdown;
 
-fn handle_connection(stream: &mut TcpStream){
-    loop{
-        let mut buffer = [0;128];
-        println!("listening fo incomming PING");
-        stream.read(&mut buffer).unwrap();
-        stream.write_all("+PONG\r\n".as_bytes()).unwrap();
-    }
-    println!("Exited loop");
+fn handle_connection(mut stream:TcpStream){
+    thread::spawn(move || {
+        let mut  noreq = 0;
+        loop{
+            let mut buffer: [u8;128] = [0;128];
+            println!("listening fo incomming PING");
+            let len: usize = stream.read(&mut buffer).unwrap();
+            let msg:String = String::from_utf8(buffer[..len].to_vec()).unwrap();
+            println!("{:?}",msg);
+            if msg == "*1\r\n$4\r\nping\r\n"{
+                stream.write_all("+PONG\r\n".as_bytes()).unwrap();
+                continue;
+            }            
+            if noreq >= 30{
+                stream.shutdown(Shutdown::Both).unwrap();
+            }else{
+                noreq += 1;
+                thread::sleep(Duration::from_secs(1));
+            }
+        }
+    });
 }
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-
                 println!("accepted new connection");
-                
-                handle_connection(&mut _stream);
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
